1b134d29ac39b101821513dca1e402c9c2daec61
ildar-G <igazimov83@gmail.com>
2023-08-18 22:53:51 +0300

    def date_format(date) in utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -9,3 +9,13 @@ def load_operations(filename):
     with open(filename, "r", encoding="UTF-8") as file:
         list_ = json.load(file)
     return list_
+
+
+def date_format(date):
+    """
+    Функция принимает строку с датой в формате "2019-08-26T10:50:58.294041"
+    и возвращет в формате 26.08.2019
+       """
+    str_date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
+    formated_date = str_date.strftime('%d.%m.%Y')
+    return formated_date
