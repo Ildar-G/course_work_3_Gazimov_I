50090df484d2e5c2dd4d79ad920028ccd4972c71
ildar-G <igazimov83@gmail.com>
2023-08-19 10:30:39 +0300

    test_utils.py add def test_load_operations()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,7 @@
 import json
 import pytest
-from utils.functions import load_operations, sort_list, get_executed, date_format, mask_card
-from main import main, FILENAME
+from utils.utils import load_operations, sort_list, get_executed, date_format, mask_card
 
 
 def test_load_operations():
-    assert type(load_operations(operations.json) == list
+    assert list == type(load_operations('../operations.json'))
