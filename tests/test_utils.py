8c0fef6df8643a56941ea050956893b00451e428
ildar-G <igazimov83@gmail.com>
2023-08-19 11:22:06 +0300

    test_utils.py add def test_get_executed()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -37,3 +37,46 @@ def test_sort_list():
 def test_date_format():
     assert date_format("2018-09-12T21:27:25.241689") == "12.09.2018"
     assert date_format("2019-07-03T18:35:29.512364") == "03.07.2019"
+
+
+def test_get_executed():
+    test_list = [
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+        },
+        {
+            "id": 863064926,
+            "state": "EXECUTED",
+            "date": "2019-12-08T22:46:21.935582",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+        }
+
+    ]
+    assert get_executed(test_list) == [
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+        },
+        {
+            "id": 863064926,
+            "state": "EXECUTED",
+            "date": "2019-12-08T22:46:21.935582",
+        },
+    ]
